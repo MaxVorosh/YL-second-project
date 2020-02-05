973fc5d8eabc973497a54f6e4187118531816436
Denittka <dan.belokon@gmail.com>
2020-02-05 21:37:33 +0300

    Турель надо будет завтра как-то сжать или пули немного сместить, иначе получается не прямо из ствола. Лист персонажа нашёл, завтра сделаю анимацию. Также завтра я почищу файлы, потому что там, как я посмотрел, есть лишнее и неиспользуемое. Я очень признателен, потому что всё это время занимался школьным проектом.

--- classes/Turret/__init__.py
+++ classes/Turret/__init__.py
@@ -4,8 +4,7 @@ import pygame
 class Turret(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load('sprites\\turret.png'), (40, 40))
+        self.image = pygame.transform.scale(pygame.image.load('sprites\\turret.jpg'), (40, 40))
         self.rect = self.image.get_rect()
         self.rect.x = x * 40
         self.rect.y = y * 40
-        # TODO Нужно найти картинку
